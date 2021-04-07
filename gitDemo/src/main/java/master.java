01ca90805c496b1aec8416fda74022f686ffc872
YXY-121 <835264630@qq.com>
2021-04-07 15:12:45 +0800

    dev和master冲突 master

--- gitDemo/src/main/java/master.java
+++ gitDemo/src/main/java/master.java
@@ -8,5 +8,7 @@ public class master {
     public void test(){
         System.out.println("我是master");
         System.out.println("dev修改");
+
+        System.out.println("dev和master的冲突 master ");//我去ideal好智能 还能自动转换分支的内容
     }
 }
