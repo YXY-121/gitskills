52c7fe4f4214a1db24f027ed2bc9a46cbec1e767
YXY-121 <835264630@qq.com>
2021-04-07 13:18:26 +0800

    丢失的dev

--- gitDemo/src/main/java/master.java
+++ gitDemo/src/main/java/master.java
@@ -8,6 +8,10 @@ public class master {
     public void test(){
         System.out.println("我是master");
         System.out.println("dev修改");
+<<<<<<< HEAD
         System.out.println("dev先提交 测试冲突。");
+=======
+        System.out.println("dev先提交 测试冲突");
+>>>>>>> 3121b58... dev测试冲突
     }
 }
